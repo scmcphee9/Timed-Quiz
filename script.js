3dc19b97491400aed896fb4be1f4bd3777650019
Shawn McPhee <scmcphee9@gmail.com>
2020-10-17 20:37:41 -0600

    added if statement to adjust user score appropriately

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 // variable stored in global memory to track score
-var userScore = 0;
+var userScore = 5;
 
 // variable for starting time of timer until end of game
-var secondsLeft = 120;
+var secondsLeft = 65;
+var oldQuestion = 0;
+var newQuestion = 1;
+var responseQ = 0;
+var responseQ2 = 2;
 
-// need event listener to start timer when start button is pushed
-
-// this variable selects the the question id
-var questions = [
-  "#question_1",
-  "#question_2",
-  "#question_3",
-  "#question_4",
-  "#question_5",
-];
-var questionCount = 0;
-
+// This is the first viewing page, and will start the timer when the button is clicked
 $("#startQuiz").removeClass("hide");
 
 $("#start").on("click", function () {
   $("#startQuiz").addClass("hide");
-  $("#question_1").removeClass("hide");
+  setTime();
+});
+
+// template literals to run through all the questions hiding and unhiding the html questions
+$("body").on("click", function (event) {
+  if (event.target.matches("button")) {
+    $(`#question_${oldQuestion}`).addClass("hide");
+    $(`#question_${newQuestion}`).removeClass("hide");
+    oldQuestion++;
+    newQuestion++;
+  }
+  // if a user selects a correct answer this will say correct on the following question, and add 10points to the users score
+  // if the player selects an incorrect answer this will remove 5 points from the score and 5 seconds from the timer
+  if (event.target.matches("#correctAnswer")) {
+    $(`#rightWrong_${responseQ}`).text("Correct!");
+    responseQ++;
+    userScore += 10;
+  } else {
+    $(`#rightWrong_${responseQ}`).text("Wrong...");
+    responseQ++;
+    userScore -= 5;
+    secondsLeft -= 5;
+  }
+  console.log(userScore);
+  $("#highScores").text(userScore);
 });
 
 // counts down timer
@@ -34,15 +51,9 @@ function setTime() {
 
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
-      // calls end game function
-      endGame();
+      // When timer =0 this will bring the player to the last screen if they run out of time
+      $("#allQuestions").addClass("hide");
+      $("#question_6").removeClass("hide");
     }
   }, 1000);
 }
-
-function endGame() {
-  // enter code to send to endgame page if timer goes to 0
-}
-
-// calls timer function
-setTime();
